df96d4e0d489bdc9c9be7b4db736f89db4799295
Iryna <irynazankovska@gmail.com>
2024-08-31 18:26:18 -0400

    deleted unused code

--- app/components/Navigation.vue
+++ app/components/Navigation.vue
@@ -55,13 +55,8 @@
 
 <script>
 export default {
-  //name: "Navigation",
-  //   const navigation = [
-  //   { name: 'Dashboard', href: '#', current: true },
-  //   { name: 'Team', href: '#', current: false },
-  //   { name: 'Projects', href: '#', current: false },
-  //   { name: 'Calendar', href: '#', current: false },
-  // ]
+  name: "Navigation",
+
   data() {
     return {
       sidebarVisible: false,
